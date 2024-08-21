ec2e4d0b7eb9da67884f19e794e29f550520766b
dilshanzz <dilshanwijewardhana999@gmail.com>
2024-08-21 13:36:24 +0530

    done

--- src/main/java/edu/icet/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.icet.repository;
-
-import edu.icet.dto.CustomerDto;
-import edu.icet.entity.CustomerEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
-
-public interface CustomerRepository extends CrudRepository<CustomerEntity,Long> {
-}
--- src/main/java/edu/icet/service/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package edu.icet.service;
-
-import edu.icet.dto.CustomerDto;
-
-public interface CustomerService {
-    CustomerDto create(CustomerDto customerDto);
-}
